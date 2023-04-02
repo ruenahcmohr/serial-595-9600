b534d4440999b7862a793e749dec64138abc7e27
root <root@ruesshop.net>
2023-04-02 15:47:28 -0700

    adding files

---
+++ message2.sh
@@ -0,0 +1,25 @@
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 2
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat blank.bin > /dev/ttyUSB1
+sleep 1
+
