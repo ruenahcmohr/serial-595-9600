30e73df4c7549947edb457d1b486f538d01e1b46
root <root@ruesshop.net>
2024-01-07 14:28:39 -0800

    circuit adjustments and schematic corrections. more precoded displays and a count script

---
+++ count.sh
@@ -0,0 +1,192 @@
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
+cat 0.bin > /dev/ttyUSB1
+sleep 1
+cat 1.bin > /dev/ttyUSB1
+sleep 1
+cat 2.bin > /dev/ttyUSB1
+sleep 1
+cat 3.bin > /dev/ttyUSB1
+sleep 1
+cat 4.bin > /dev/ttyUSB1
+sleep 1
+cat 5.bin > /dev/ttyUSB1
+sleep 1
+cat 6.bin > /dev/ttyUSB1
+sleep 1
+cat 7.bin > /dev/ttyUSB1
+sleep 1
+cat 8.bin > /dev/ttyUSB1
+sleep 1
+cat 9.bin > /dev/ttyUSB1
+sleep 1
+cat A.bin > /dev/ttyUSB1
+sleep 1
+cat B.bin > /dev/ttyUSB1
+sleep 1
+cat C.bin > /dev/ttyUSB1
+sleep 1
+cat D.bin > /dev/ttyUSB1
+sleep 1
+cat E.bin > /dev/ttyUSB1
+sleep 1
+cat F.bin > /dev/ttyUSB1
+sleep 1
